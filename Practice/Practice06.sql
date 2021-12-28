b7583d85e8fc4fc34037345fac710df632fe240c
chaerin <77728068+Boram8878@users.noreply.github.com>
2021-12-29 01:00:32 +0900

    practice06 select insert update delete

--- Practice/Practice06.sql
+++ Practice/Practice06.sql
@@ -3,6 +3,7 @@ drop table book;
 drop table author;
 
 drop sequence seq_book_id;
+drop sequence seq_author_id;
 
 --시퀀스 만들기 1부터 시작해서  1씩 증가하는 seq_book_id 라는 이름의 시퀀스 book_id/author_id에 사용할 시퀀스
 create sequence seq_book_id
@@ -10,7 +11,7 @@ increment by 1
 start with 1
 nocache;
 
-create sequence seq_aut_id
+create sequence seq_author_id
 increment by 1
 start with 1
 nocache;
@@ -22,7 +23,7 @@ select seq_book_id.currval from dual;
 
 --필요한 테이블 book / author
 create table author(
-     author_id number(5), 
+     author_id number(5), --데이터가 들어갈 형식들 써줘야함. 
      author_name varchar2(10) not null,
      author_desc varchar2(50),
      primary key(author_id) --author id로 primary key 만들어주기
@@ -30,9 +31,9 @@ create table author(
 
 create table book(
      book_id number(5),
-     title varchar2(50) not null,
-     pubs varchar2(20),
-     pub_date date,
+     book_title varchar2(50) not null,
+     book_pubs varchar2(20),
+     book_pub_date date,
       author_id number(5), 
      primary key(book_id),
      constraint book_fk foreign key(author_id) --author의 author_id - pk와 book의 author_id -fk 연결해주기
@@ -41,24 +42,94 @@ create table book(
 
 --데이터 넣기 시퀀스 사용
 
+/********************************
+211227 author insert문 테스트 
+********************************/
+select *
+from author;
+
+select *
+from book;
+
+select *
+from user_sequences;
+
+rollback;
+
+---------------------book/author테이블조인-------------------------------
+select au.author_name,
+       au.author_desc,
+       bo.book_id,
+       bo.book_title,
+       bo.book_pubs,
+       to_char(bo.book_pub_date, 'yyyy-mm-dd') bookPdate,
+       bo.author_id
+from author au, book bo
+where au.author_id = bo.author_id;
+
+--------------------------------------------
+--select author
+select author_id,
+       author_name,
+       author_desc
+from author;
+
+--select book
+select book_id,
+       book_title,
+       book_pubs,
+       to_char(book_pub_date, 'yyyy-mm-dd'),
+       author_id
+from book;
+--------------------------------------------
+
+--delete author
+delete from author
+where author_id = 1;
+
+--delete book
+delete from book
+where author_id = 3;
+---------------------------------------------
+
+--author update
+update author
+set author_name = '김문열',
+    author_desc = '삼국지 작가'--여기까지만 쓰면 모든 데이터가 김문열/삼국지 작가 로 바뀌게됨 의도한게 아니라면 이렇게쓰지말기
+where author_id =1; 
+
+--book update 
+/*
+update book
+set book_pubs = '삼국국'
+where book_id = 2;
+*/
+
+--book update author_id바꾸기
+update book
+set author_id = 2
+where book_title = '토지';
+
+-----------------------------------------------
+
 --author 테이블 데이터 넣기
 insert into author
-values(seq_aut_id.nextval,'이문열','경북 영양');
+values(seq_author_id.nextval,'이문열','경북 영양');
 
 insert into author
-values(seq_aut_id.nextval,'박경리','경상남도 통영');
+values(seq_author_id.nextval,'박경리','경상남도 통영');
 
 insert into author
-values(seq_aut_id.nextval,'유시민','17대 국회의원');
+values(seq_author_id.nextval,'유시민','17대 국회의원');
 
 insert into author
-values(seq_aut_id.nextval,'기안84','기안동에서 산 84년생');
+values(seq_author_id.nextval,'기안84','기안동에서 산 84년생');
 
 insert into author
-values(seq_aut_id.nextval,'강풀','온라인 만화가 1세대');
+values(seq_author_id.nextval,'강풀','온라인 만화가 1세대');
 
 insert into author
-values(seq_aut_id.nextval,'김영하','알쓸신잡');
+values(seq_author_id.nextval,'김영하','알쓸신잡');
 
 
 --book 테이블 데이터 넣기
